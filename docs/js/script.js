eb1bfb9df945679034e480fe71d97eee3481b48d
tom-weh <tom.wehmeyer@bertelsmann.de>
2019-11-26 11:34:04 +0100

    Polished and extended page

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,15 +1,43 @@
 function toggleNav(){
   var x = document.getElementById("expandable_menu");
-  // console.log("is open is: " + isOpen)
   if (x.classList.contains("block")) document.getElementById("expandable_menu").classList.replace("block", "hidden")
-  else {document.getElementById("expandable_menu").classList.replace("hidden","block")
-  console.log(2)
-}
-  // // document.getElementById("expandable_menu").classList.add("block");
-  // if (x.style.display === "none") {
-  //   x.style.display = "block";
-  // } else {
-  //   x.style.display = "none";
-  // }
+  else {
+    document.getElementById("expandable_menu").classList.replace("hidden","block")
+  }
 }
 
+
+function countText(){
+  //getText
+  text = document.getElementById("word-counter-textarea").value;
+
+  var words = 0;
+  var splitted_words = text.split(" ");
+  var splitted_sentences = text.split(/[.!?]/);
+
+  for (i in splitted_words){
+    word = splitted_words[i];
+    if (word !== "") {
+      words += 1;
+    }
+  }
+
+  var characters = text.length;
+
+  var sentences = 0;
+  for (i in splitted_sentences){
+    sentence = splitted_sentences[i].replace(" ", "");
+    if (sentence !== "") {
+      sentences += 1;
+    }
+  }
+  
+  var paragraphs = 0;
+  if (words !== 0) paragraphs = text.split("\n").length;
+
+  td = document.getElementById("text-details").getElementsByTagName("td");
+  td[0].innerHTML = words;
+  td[1].innerHTML = characters;
+  td[2].innerHTML = sentences;
+  td[3].innerHTML = paragraphs;
+}
